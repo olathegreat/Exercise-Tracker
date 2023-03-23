dcacd6e7e6af8c3be57945f1fb78529b0a0acca9
olarotimi <ademolarotimi2000@gmail.com>
2023-03-23 16:09:49 +0100

    feat

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,4 +1,5 @@
 const router =  require('express').Router();
+const Exercise = require('../models/exercise.model');
 let User = require('../models/exercise.model');
 
 router.route('/').get((req, res)=>{
@@ -12,10 +13,10 @@ router.route('/add').post((req, res)=>{
     const username = req.body.username;
     const description = req.body.description;
     const duration = Number(req.body.duration);
-    const date = Date.parse(req.body.username);
+    const date = Date.parse(req.body.date);
 
 
-    const newExercie = new  Exercise({
+    const newExercise = new  Exercise({
         username,
         description,
         duration,
@@ -26,7 +27,34 @@ router.route('/add').post((req, res)=>{
     newExercise.save()
      .then(()=> res.json('Exercise  added!'))
      .catch(err => res.status(400).json('Error: ' + err));
-})
+});
+
+router.route('/:id').get((req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('error: ' + err))
+});
+
+router.route('/:id').delete((req,res)=>{
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(()=> res.json('Exercise deleted'))
+    .catch(err => res.status(400).json('error: ' + err))
+});
+
+router.route('/update/:id').post((req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+          .then(()=> res.json('Éxercise Updated'))
+          .catch(err=> res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('error: ' + err))
+});
 
 
 module.exports = router;
